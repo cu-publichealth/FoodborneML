3c5afac505819eb74a945fa5cf99973fb19b2d30
Tom Effland <tom.effland@gmail.com>
2016-02-02 16:52:48 -0500

    [ci skip] added generic settings file

---
+++ foodbornenyc/settings.py
@@ -0,0 +1,27 @@
+"""
+Global Settings
+"""
+
+database_config = {
+    'user': '<yourdbname>',
+    'password': '<yourdbpassword>',
+    'dbhost': '<yourdbhost>'
+}
+
+yelp_download_config = {
+    'rawdata_dir': 'foodbornenyc/sources/yelpfiles/', # the dir to download the yelp data from S3 to
+    'local_file':  'yelp_businesses.json.gz',
+    'bucket_name': 'yelp-syndication',
+    'bucket_dir':  'nychealth'
+}
+
+geocode_config = {
+    'wait_time': 4, # wait 4 sec until timeout
+    'run_time': 240 # run for max 4 hours
+}
+
+yelp_classify_config = {
+    'model_file': 'foodbornenyc/pipelines/models/yelp_sick_logreg.pkl',
+    'days_back': 7,
+    'verbosity': 1
+}

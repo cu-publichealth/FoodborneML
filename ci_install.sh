9b54e1ac4758ae94d2edaa3a546547188c909532
Tom Effland <tom.effland@gmail.com>
2016-02-04 09:44:08 -0500

    try w/o new venv

--- ci_install.sh
+++ ci_install.sh
@@ -2,9 +2,9 @@
 
 # leave the virtualenv and create a new one with the system packages cached on apt-get
 #sudo apt-get install libblas-dev liblapack-dev libatlas-dev gfortran libatlas3gf-base python-numpy python-scipy
-deactivate
-virtualenv --system-site-packages testvenv
-source testvenv/bin/activate
+# deactivate
+# virtualenv --system-site-packages testvenv
+# source testvenv/bin/activate
 
 # now install the requirements from pip
 pip install -r travis_requirements.txt

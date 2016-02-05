ca291b65cf9dae505c0ca43cd43be8da9bfe8668
Tom Effland <tom.effland@gmail.com>
2016-02-05 14:37:50 -0500

    Updated pipelines

---
+++ foodbornenyc/pipelines/pipeline_base.py
@@ -0,0 +1,7 @@
+"""
+The pipeline base object
+
+Currently unimplemented but if we decide that pipelines used in the project
+should have certain properties/checks that scikit's doesn't provide,
+we can implement our own extension of it here
+"""

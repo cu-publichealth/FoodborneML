462a42f8bf43974f11d0a34bb440ee65e7c2eb2f
teffland <tom.effland@gmail.com>
2016-02-07 17:37:18 -0500

    no more notebooks

--- foodbornenyc/sources/twitter_search.py
+++ foodbornenyc/sources/twitter_search.py
@@ -14,12 +14,14 @@ from foodbornenyc.util.util import sec_to_hms, get_logger
 logger = get_logger(__name__, level="INFO")
 
 def make_query(twitter, keywords):
+    """ Take keywords and Twython object and return back the statuses"""
     query = ' OR '.join(keywords)
     results = twitter.search(q=query)
     tweets = results['statuses']
     return tweets
 
 def tweets_to_Tweets(tweet_list, select_fields):
+    """ Take list of json from twitter and make into list of Tweets objects"""
     tweets = []
 #     print select_fields
     for tweet in tweet_list:
@@ -39,6 +41,7 @@ search_terms = [
     '"the runs"'
 ]
 
+# all possible fields from twitter
 fields = [
 #         'contributors', #<type 'NoneType'>
 #         'truncated', #<type 'bool'>
@@ -74,12 +77,15 @@ def query_twitter(how_long=0):
                       twitter_config['access_token'],
                       twitter_config['access_token_secret'])
     db = get_db_session()
+
     # can send 180 requests per 15 min = 5 sec
     request_wait = 5
     start = time.time()
+
+    # make sure we don't create duplicates. 
+    # keeping track of this ourselves saves many db hits
     id_set = set([ t.id for t in db.query(Tweet.id).all() ])
     # if we don't specify go indefinitely
-    search_count = 0
     while not (how_long) or (time.time()-start < how_long):
         tweets = make_query(twitter, search_terms)
         logger.info("%i Total unique tweets in %i:%i:%i time", len(id_set), *sec_to_hms(time.time()-start))

32b858c42ac17eb9c4686301435ab886adf86c27
teffland <tom.effland@gmail.com>
2016-02-08 13:14:31 -0500

    db_settings.py

--- foodbornenyc/db_settings.py
+++ foodbornenyc/db_settings.py
@@ -9,8 +9,8 @@ database_config = {
 }
 
 twitter_config = {
-    'consumer_key': 'BqyVMEVyJ76DLUwwwB6sG7mc8',
-    'consumer_secret': '803Ztn45ruCVzUhhehOBwvhwKdmxHTOTZp1TiAYYcXRneuYUsw',
-    'access_token': '4211115641-bpkKfPDsiujLZYRz2OlaK9YwTHRiaL35jv7pGo3',
-    'access_token_secret': 'usEKY68eRfP85QkgWy69hAsF7j7xM6sq7FM3olRbdVZRO'
+    'consumer_key': '',
+    'consumer_secret': '',
+    'access_token': '',
+    'access_token_secret': ''
 }

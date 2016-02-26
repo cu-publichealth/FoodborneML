5a83fe524fd6df09a4e51cb325a7358eb5229106
Steven <ys2833@columbia.edu>
2016-02-26 03:15:30 -0500

    pytests for all twitter functions

--- foodbornenyc/test/sources/test_twitter_search.py
+++ foodbornenyc/test/sources/test_twitter_search.py
@@ -1,9 +1,11 @@
-import unittest
-from mock import Mock
+import pytest
+from mock import Mock, patch
 
 import foodbornenyc.sources.twitter_search as twitter_search
-from foodbornenyc.util.util import xuni
+
 from foodbornenyc.models.documents import Tweet
+from foodbornenyc.models.models import get_db_session
+from foodbornenyc.test.generate_db import clear_tables
 from twython.exceptions import TwythonError
 
 sample = [ {
@@ -26,39 +28,55 @@ sample = [ {
             'place': None
         }]
 
-class TestTwitterSource(unittest.TestCase):
+def test_make_query():
+    """ The correct query should be requested """
+    keywords = ['food', 'stomach', 'disease']
+    tweets = {'statuses': sample}
 
-    def test_make_query(self):
-        """ Test a sample query """
-        keywords = ['food', 'stomach', 'disease']
-        tweets = {'statuses': sample}
+    twitter = Mock()
+    twitter.search = Mock(return_value=tweets)
 
-        twitter = Mock()
-        twitter.search = Mock(return_value=tweets)
+    query = twitter_search.make_query(twitter, keywords)
 
-        query = twitter_search.make_query(twitter, keywords)
+    twitter.search.assert_called_with(q='food OR stomach OR disease')
+    assert query == sample
 
-        twitter.search.assert_called_with(q='food OR stomach OR disease')
-        self.assertEqual(query, sample)
+def test_make_query_failed():
+    """ If there's an exception, fail gracefully """
+    twitter = Mock()
+    twitter.search = Mock(side_effect=TwythonError("Can't reach Twitter"))
 
-    def test_make_query_failed(self):
-        """ Make Query should fail gracefully """
-        twitter = Mock()
-        twitter.search = Mock(side_effect=TwythonError("Can't reach Twitter"))
+    query = twitter_search.make_query(twitter, ['food'])
 
-        query = twitter_search.make_query(twitter, ['food'])
+    assert query == []
 
-        self.assertEqual(query, [])
+def test_tweets_to_Tweets():
+    """ Sample tweets should be converted properly """
+    fields = ['text', 'id_str']
+    tweets = twitter_search.tweets_to_Tweets(sample, fields)
+    assert_matching_tweets
 
-    def test_tweets_to_Tweets(self):
-        """ Convert sample tweets """
-        fields = ['text', 'id_str']
-        tweets = twitter_search.tweets_to_Tweets(sample, fields)
+@patch('foodbornenyc.sources.twitter_search.Twython')
+def test_query_twitter(twython):
+    """ Database should be filled with tweets returned from Twython """
+    clear_tables()
 
-        self.assertEqual(len(tweets), 2)
-        self.assertEqual(tweets[0].text, sample[0]['text'])
-        self.assertEqual(tweets[0].id, sample[0]['id_str'])
-        self.assertEqual(tweets[1].text, sample[1]['text'])
-        self.assertEqual(tweets[1].id, sample[1]['id_str'])
+    # make twitter return the sample tweets
+    tweet_sequence = [{'statuses': [sample[0]]}, {'statuses': [sample[1]]}]
+    twitter = twython.return_value
+    twitter.search = Mock(side_effect=tweet_sequence)
+
+    twitter_search.query_twitter(7)
+
+    assert len(twitter.search.call_args_list) == 2
+    assert_matching_tweets(get_db_session().query(Tweet).all())
+
+def assert_matching_tweets(tweets):
+    """ Helper method for checking if tweets match sample """
+    assert len(tweets) == 2
+    assert tweets[0].text == sample[0]['text']
+    assert tweets[0].id == sample[0]['id_str']
+    assert tweets[1].text == sample[1]['text']
+    assert tweets[1].id == sample[1]['id_str']
 
 

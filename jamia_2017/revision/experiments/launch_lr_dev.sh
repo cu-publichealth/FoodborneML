8a2fca952dd89bf15d2592efd9c31f010cd5ab34
teffland <tom.effland@gmail.com>
2017-06-28 00:02:53 -0400

    basline dev experiments set up

---
+++ jamia_2017/revision/experiments/launch_lr_dev.sh
@@ -0,0 +1,4 @@
+nohup lr_sick_gold_dev.py > lr_sick_gold_dev.out &
+nohup lr_sick_silver_dev.py > lr_sick_silver_dev.out &
+nohup lr_mult_gold_dev.py > lr_mult_gold_dev.out &
+nohup lr_mult_silver_dev.py > lr_mult_silver_dev.out &
---
+++ jamia_2017/revision/experiments/launch_svm_dev.sh
@@ -0,0 +1,4 @@
+nohup svm_sick_gold_dev.py > svm_sick_gold_dev.out &
+nohup svm_sick_silver_dev.py > svm_sick_silver_dev.out &
+nohup svm_mult_gold_dev.py > svm_mult_gold_dev.out &
+nohup svm_mult_silver_dev.py > svm_mult_silver_dev.out &

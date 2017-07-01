bc2034d23abcd5dbda607391e063079798a8a01a
teffland <tom.effland@gmail.com>
2017-07-01 06:51:45 -0400

    biased experiments also

---
+++ jamia_2017/revision/experiments/cnn_mult_gold_dev.py
@@ -0,0 +1,24 @@
+import numpy.random as npr
+import numpy as np
+from chainer_monitor.run_experiment import run_experiment
+from chainer_experiment_util import chainer_random_search
+
+base = yaml.load(open('cnn_setup_mult_gold_config.yaml'))
+N = 50
+random_points = {
+    'model_setup':{
+        'setup_config':{
+            'cnn_ngrams':[range(1,n) for n in npr.choice([2,3,4], N)],
+            'cnn_n_filters_per':npr.randint(10,50, N),
+            'dropout_prob':npr.uniform(.1,.4, N)
+        }
+    },
+    'trainer_setup':{
+        'setup_config': {
+            'adam_alpha':npr.uniform(1e-3, 1e-2, N),
+            'l2_coef':[10.**e for e in npr.uniform(-3,1, N)]
+        }
+    }
+}
+
+chainer_random_search(base, random_points, N)

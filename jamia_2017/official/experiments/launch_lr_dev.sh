ce8664b9d01f8e50044467aa35d4bfe4c74ae659
teffland <tom.effland@gmail.com>
2017-10-08 13:39:51 -0400

    offical jamia repo

--- jamia_2017/official/experiments/launch_lr_dev.sh
+++ jamia_2017/official/experiments/launch_lr_dev.sh
@@ -1,4 +1,6 @@
+nohup python lr_sick_biased_dev.py > lr_sick_biased_dev.out &
 nohup python lr_sick_gold_dev.py > lr_sick_gold_dev.out &
 nohup python lr_sick_silver_dev.py > lr_sick_silver_dev.out &
+nohup python lr_mult_biased_dev.py > lr_mult_biased_dev.out &
 nohup python lr_mult_gold_dev.py > lr_mult_gold_dev.out &
 nohup python lr_mult_silver_dev.py > lr_mult_silver_dev.out &

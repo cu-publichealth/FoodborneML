83e5102831aac6d6ccb65c8944f6a7d6206a1f59
teffland <tom.effland@gmail.com>
2017-10-08 12:53:03 -0400

    jamia revision final

--- jamia_2017/revision/experiments/launch_lr_dev.sh
+++
@@ -1,4 +0,0 @@
-nohup python lr_sick_gold_dev.py > lr_sick_gold_dev.out &
-nohup python lr_sick_silver_dev.py > lr_sick_silver_dev.out &
-nohup python lr_mult_gold_dev.py > lr_mult_gold_dev.out &
-nohup python lr_mult_silver_dev.py > lr_mult_silver_dev.out &
--- jamia_2017/revision/experiments/launch_svm_dev.sh
+++
@@ -1,4 +0,0 @@
-nohup python svm_sick_gold_dev.py > svm_sick_gold_dev.out &
-nohup python svm_sick_silver_dev.py > svm_sick_silver_dev.out &
-nohup python svm_mult_gold_dev.py > svm_mult_gold_dev.out &
-nohup python svm_mult_silver_dev.py > svm_mult_silver_dev.out &
